a3cf30802088c4eea0b4833df49daf6c2bbbbd0e
Ishan <ishanm6@gmail.com>
2017-07-09 23:59:49 +0530

    ada
    ada

---
+++ Assignment3.4/src/Example.java
@@ -0,0 +1,43 @@
+class Student
+{
+private int name,phoneNumber,rollNumber;
+private String year;
+
+public int getName()
+{
+    return name;
+}
+public int getPhoneNumber()
+{
+    return phoneNumber;
+    
+}
+public int getRollNumber()
+{
+    return rollNumber;
+    
+}
+public String getYear()
+{
+return year;
+}
+        public void setName( int newName) {
+      name = newName;
+}
+        public void setPhoneNumber( int newPhoneNumber) {
+         phoneNumber = newPhoneNumber;
+        }
+         public void setRollNumber( int newRollNumber) {
+         rollNumber = newRollNumber;
+         }
+        public void setYear( String newYear) {
+         year = newYear;
+        }
+        
+public class Example {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
